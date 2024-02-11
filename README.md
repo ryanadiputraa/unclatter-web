fb3f6ba68b2521c2da448ab129c22b2c4f19bea5
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-02-11 21:00:45 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# UnClatter
+
+UnClatter is an article bookmarking tool that also removes distractions like ads and popups. It offers a clean reading experience, allowing users to focus solely on the essential content.
+
+## Development
+
+Adjust your `.env.development` file
+
+install dependencies:
+
+```bash
+yarn
+```
+
+start the development server:
+
+```bash
+yarn dev
+```
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#130e01',
+        'bg': '#efefef',
+        'primary': '#0042db',
+        'secondary': '#98b3c5',
+        'accent': '#005475',
+        'text-dark': '#fef9ec',
+        'bg-dark': '#141412',
+        'primary-dark': '#2465ff',
+        'secondary-dark': '#acb9cf',
+        'accent-dark': '#8adeff',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
